c51bdea712d04cebef63d7269c7d4d7370249c38
Armando Eigo Pimentel <119333543+Armandoeigo@users.noreply.github.com>
2026-08-11 10:09:07 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="Painel do Extrator", page_icon="🏛️")
+
+st.title("🏛️ Bem-vindo ao Painel do Extrator")
+st.write("Prefeitura Municipal de Salvador")
+st.markdown("---")
+st.write("👈 **Selecione uma das ferramentas no menu lateral esquerdo para começar:**")
+st.write("- 📄 **Relatórios em Texto:** Para visualizar Decretos, anexos longos e Organogramas.")
+st.write("- 📊 **Planilha Excel:** Para gerar tabelas matemáticas perfeitas de Cargos Acrescidos e Suprimidos.")
